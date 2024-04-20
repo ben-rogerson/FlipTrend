b66c5eb92d3c2eb628895a36419f0ee82c26ed69
Ben Rogerson <info@benrogerson.dev>
2024-04-20 17:24:26 +0930

    Lintfix

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,7 @@ import '@/global.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
