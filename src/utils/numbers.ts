3dca972556d3f7ef61c46f165057f1da3ae011f3
Ben Rogerson <info@benrogerson.dev>
2024-04-20 18:27:15 +0930

    Remove trailing zeros in marketcaps

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -5,29 +5,33 @@
  * @example
  * ```ts
  * const num = 1000000
- * const { abbrNumber, abbrWords } = getAbbreviatedNumber(num) // '1m'
+ * const { abbrNumber, abbrWords } = getAbbreviatedNumber(num)
+ * console.log(abbrNumber) // => '1m'
+ * console.log(abbrWords) // => '1 million'
  * ```
  */
 export const getAbbreviatedNumber = (num: number) => {
   if (num >= 1_000_000_000_000) {
-    const abbrNum = (num / 1_000_000_000_000).toFixed(1)
+    const abbrNum = formatNumber(num / 1_000_000_000_000)
     return { abbrNumber: `${abbrNum}t`, abbrWords: `${abbrNum} trillion` }
   }
 
   if (num >= 1_000_000_000) {
-    const abbrNum = (num / 1_000_000_000).toFixed(1)
+    const abbrNum = formatNumber(num / 1_000_000_000)
     return { abbrNumber: `${abbrNum}b`, abbrWords: `${abbrNum} billion` }
   }
 
   if (num >= 1_000_000) {
-    const abbrNum = (num / 1_000_000).toFixed(1)
+    const abbrNum = formatNumber(num / 1_000_000)
     return { abbrNumber: `${abbrNum}m`, abbrWords: `${abbrNum} million` }
   }
 
   if (num >= 1_000) {
-    const abbrNum = (num / 1_000).toFixed(1)
+    const abbrNum = formatNumber(num / 1_000)
     return { abbrNumber: `${abbrNum}k`, abbrWords: `${abbrNum} thousand` }
   }
 
   return { abbrNumber: num.toString(), abbrWords: num.toString() }
 }
+
+const formatNumber = (num: number) => num.toFixed(1).replace(/\.0$/, '')
