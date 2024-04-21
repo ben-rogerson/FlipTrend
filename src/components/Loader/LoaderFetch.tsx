e5bb7ef4848944ee4bfa6b6736e36b84269287d4
Ben Rogerson <info@benrogerson.dev>
2024-04-21 21:39:41 +0930

    Safari gradient icon fix

--- src/components/Loader/LoaderFetch.tsx
+++ src/components/Loader/LoaderFetch.tsx
@@ -7,7 +7,7 @@ export const LoaderFetch = (props: { isLoading: boolean }) => (
       className={cn(
         '@xs/loader:px-46 relative mx-auto grid w-fit items-center gap-3 bg-page px-5 text-center font-heading font-bold @sm/loader:text-2xl @lg/loader:flex @lg/loader:text-left @lg/loader:text-4xl @lg/loader:tracking-tight @xl/loader:px-10',
         props.isLoading
-          ? 'opacity-100 duration-1000 animate-in fade-in-0'
+          ? 'opacity-100 duration-200 animate-in fade-in-0'
           : 'opacity-0'
       )}
     >
