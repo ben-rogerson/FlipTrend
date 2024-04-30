d87408303fe2e026fb1eca2009e39ba66aa6126f
Ben Rogerson <info@benrogerson.dev>
2024-04-30 11:27:01 +0930

    Remove animation on large blocks

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -7,10 +7,10 @@ export const BaseLayout = (props: { children: ReactNode }) => (
     <header className="duration-700 ease-out animate-in fade-in-0 slide-in-from-top-3">
       <Header />
     </header>
-    <main className="mx-auto max-w-screen-2xl pt-5 delay-200 duration-700 ease-out animate-in fade-in-0 slide-in-from-top-1 fill-mode-backwards padded-x">
+    <main className="mx-auto max-w-screen-2xl pt-5 duration-700 ease-out animate-in fade-in-0 slide-in-from-top-1 fill-mode-backwards padded-x">
       {props.children}
     </main>
-    <footer className="border-t-2 py-5 delay-200 duration-700 animate-in fade-in-0 fill-mode-backwards margined-x md:py-10">
+    <footer className="border-t-2 py-5 duration-700 animate-in fade-in-0 fill-mode-backwards margined-x md:py-10">
       <Footer />
     </footer>
   </div>
